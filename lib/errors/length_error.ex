1e2e1dbb5c9fd7b39c17e437ccf4fc4fc263dd2e
ryan senn <ryan.senn.au@gmail.com>
2020-02-02 16:57:21 +1000

    fix typo

--- lib/errors/length_error.ex
+++ lib/errors/length_error.ex
@@ -3,7 +3,7 @@ defmodule Avetmiss.Errors.LengthError do
 
   @impl true
   def exception({length, value}) do
-    msg = "Value exceeds field length. Lenght: #{length}, Value: #{value}"
+    msg = "Value exceeds field length. Length: #{length}, Value: #{value}"
     %Avetmiss.Errors.LengthError{message: msg}
   end
 end
